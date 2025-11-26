62242740ba63a94b4c65c3f0125913ec7bc36215
abdo <wevaacademy@gmail.com>
2025-11-26 13:44:30 -0800

    Initial commit

--- src/modules/auth/service/authontecation.service.js
+++ src/modules/auth/service/authontecation.service.js
@@ -1610,7 +1610,7 @@ export const getMyActiveOrdersForDelivery = asyncHandelr(async (req, res, next) 
 
     return res.status(200).json({
         success: true,
-        message: "✅ تم جلب الطلبات النشطة بنجاح",
+        message: "✅ تم جلب الطلبات النشطة ياعم الزفت انت =",
         count: formattedOrders.length,
         data: formattedOrders
     });
